0aafdccb43de10370fb83068df76445ece7aed4f
Marcio Ramires <marcioramiresgt@gmail.com>
2023-04-22 20:24:39 -0300

    basic counting created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading;
+
+Start();
+
+static void Start()
+{
+    int time = 10;
+    int currentTime = 0;
+
+    while (currentTime != time) 
+    {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+}
