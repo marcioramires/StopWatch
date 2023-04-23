4629116f5cec9ed9c1957b4ceea766d4f6fbd413
Marcio Ramires <marcioramiresgt@gmail.com>
2023-04-22 21:06:21 -0300

    finished

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Start(6);
+﻿Menu();
 
 static void Menu()
 {
@@ -8,7 +8,30 @@ static void Menu()
     Console.WriteLine("0 = Sair");
     Console.Write("Quanto tempo deseja contar?");
 
-    string data = Console.ReadLine();
+    string data = Console.ReadLine().ToLower();
+    char type = char.Parse(data.Substring((data.Length - 1),1));
+    int time = int.Parse(data.Substring(0,(data.Length - 1)));
+    int multiplier = 1;
+
+    if(type == 'm') multiplier = 60;
+    if(time == 0) System.Environment.Exit(0); 
+
+    PreStart(time * multiplier);
+}
+
+static void PreStart(int time)
+{
+    Console.Clear();
+    Console.WriteLine("Ready...");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Set...");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Go...");
+    Thread.Sleep(2000);
+
+    Start(time);
 }
 
 static void Start(int time)
@@ -26,4 +49,5 @@ static void Start(int time)
     Console.Clear();
     Console.WriteLine("Stopwatch finalizado!");
     Thread.Sleep(2500);
+    Menu();
 }
